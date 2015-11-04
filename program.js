657aab0d16ff585db7333134a0b53496ca761e57
Tomas Ibarra <tomas.ibarrab@gmail.com>
2015-11-03 21:45:32 -0700

    nodes

--- program.js
+++ program.js
@@ -246,3 +246,14 @@ var walk_the_DOM = function walk(node, func) {
 		node = node.nextSibling;
 	}
 };
+
+var getElementsByAttribute = function (att, value) {
+	var results = [];
+	 walk_the_DOM(document.body, function (node) {
+	 	var actual = node.nodeType === 1 && node.getAttribute(att);
+	 	if (typeof actual === 'string' && (actual === value || typeof value !== 'string')) {
+	 		results.push(node);
+	 	}
+	 });
+    return results;
+};
